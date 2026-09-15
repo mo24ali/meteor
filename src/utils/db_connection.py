79b8d3e08edc75f2d871c869e76b957ddefc52f7
mo24ali <alichere1234@gmail.com>
2026-09-15 11:28:48 +0100

    feat: add database session management utilities

---
+++ src/utils/db_connection.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from contextlib import contextmanager
+from src.utils.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    echo=False
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_engine():
+    return engine
+
+@contextmanager
+def get_db() -> Session:
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
